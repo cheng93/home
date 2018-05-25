eefb339402b88541cccd128a9f4d63ccdf9ee691
cheng93 <derek.c@hotmail.co.uk>
2018-05-25 21:06:08 +0100

    header

---
+++ src/app.ts
@@ -0,0 +1,7 @@
+import fontawesome from '@fortawesome/fontawesome';
+import faGithub from '@fortawesome/fontawesome-free-brands/faGithub';
+import faLinkedinIn from '@fortawesome/fontawesome-free-brands/faLinkedinIn';
+
+fontawesome.library.add(faGithub, faLinkedinIn);
+
+import './app.scss';
