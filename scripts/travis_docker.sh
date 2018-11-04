2b4e31e8b4a8cd5c5266e2028545b6c2104a85b5
cheng93 <derek.c@hotmail.co.uk>
2018-11-04 17:35:32 +0000

    adjust travis script

--- scripts/travis_docker.sh
+++ scripts/travis_docker.sh
@@ -1,23 +1,13 @@
 #!/bin/bash
 
-if [ "$TRAVIS_BRANCH" == "develop" ]
-then
-    docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD" $DOCKER_REGISTRY;
-    docker tag home $DOCKER_REPO/home:dev;
-    docker push $DOCKER_REPO/home:dev;
-elif [ "$TRAVIS_BRANCH" == "master" ]
+if [ "$TRAVIS_BRANCH" == "master" ]
 then
     docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD" $DOCKER_REGISTRY;
     docker tag home $DOCKER_REPO/home:latest;
     docker push $DOCKER_REPO/home:latest;
-elif [[ "$TRAVIS_BRANCH" == release/* ]]
+elif [ "$TRAVIS_TAG" == "$TRAVIS_BRANCH" ]
 then 
     docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD" $DOCKER_REGISTRY;
-    docker tag home $DOCKER_REPO/home:${TRAVIS_BRANCH:8};
-    docker push $DOCKER_REPO/home:${TRAVIS_BRANCH:8};
-elif [[ "$TRAVIS_BRANCH" == hotfix/* ]]
-then 
-    docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD" $DOCKER_REGISTRY;
-    docker tag home $DOCKER_REPO/home:${TRAVIS_BRANCH:7};
-    docker push $DOCKER_REPO/home:${TRAVIS_BRANCH:7};
+    docker tag home $DOCKER_REPO/home:${TRAVIS_TAG};
+    docker push $DOCKER_REPO/home:${TRAVIS_TAG};
 fi
